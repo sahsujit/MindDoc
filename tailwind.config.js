2a497c5e65fd143c4ccf4b4247341a72515dff3c
sujit sah <sujit8877123sah@gmail.com>
2023-08-26 15:02:52 +0545

    MindDoc

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mulish:["mulish", "sans-serif"]
+      },
+
+      colors: {
+        colorimg: "#1e3a63",
+        buttoncolor: "#4c7f7f",
+        navbg: "rgba(246, 240, 237, 0.01)",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+        deepBlue: "#02042a",
+
+
+      },
+    },
+  },
+  plugins: [],
+}
+
